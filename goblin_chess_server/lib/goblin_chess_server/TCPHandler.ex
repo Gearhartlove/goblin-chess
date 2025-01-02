9af3089887c082928dfc75bb8bba4a57ef087d9d
kristoff <kristoffgearhartfinley@gmail.com>
2025-01-02 07:39:33 -0700

    first binary pattern matching

--- goblin_chess_server/lib/goblin_chess_server/TCPHandler.ex
+++ goblin_chess_server/lib/goblin_chess_server/TCPHandler.ex
@@ -2,9 +2,19 @@ defmodule GoblinChessServer.TCPHandler do
   use ThousandIsland.Handler
 
   @impl ThousandIsland.Handler
+  def handle_data(<<first, rest::binary>> = data, socket, state) do
+    ThousandIsland.Socket.send(socket, data)
+  
+    IO.puts("first=#{inspect(first)} rest=#{inspect(rest)}")
+    {:continue, state}
+  end
+
+
+  @impl ThousandIsland.Handler
   def handle_data(data, socket, state) do
     ThousandIsland.Socket.send(socket, data)
     IO.puts("data=#{inspect(data)} socket=#{inspect(socket)} state=#{inspect(state)}")
     {:continue, state}
   end
 end
+
