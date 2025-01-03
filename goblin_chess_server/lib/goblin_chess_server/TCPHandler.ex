8ea9698817abd0b9dc4b74b3cc07774a565059cd
kristoff <kristoffgearhartfinley@gmail.com>
2025-01-02 23:11:23 -0700

    perform first binary pattern match for MOVE

--- goblin_chess_server/lib/goblin_chess_server/TCPHandler.ex
+++ goblin_chess_server/lib/goblin_chess_server/TCPHandler.ex
@@ -2,18 +2,18 @@ defmodule GoblinChessServer.TCPHandler do
   use ThousandIsland.Handler
 
   @impl ThousandIsland.Handler
-  def handle_data(<<first, rest::binary>> = data, socket, state) do
+  def handle_data(<<"MOVE", move::binary-size(1)>> = data, socket, state) do
     ThousandIsland.Socket.send(socket, data)
   
-    IO.puts("first=#{inspect(first)} rest=#{inspect(rest)}")
+    IO.puts("move=#{inspect(move)}")
     {:continue, state}
   end
 
-
   @impl ThousandIsland.Handler
-  def handle_data(data, socket, state) do
+  def handle_data(<<opt::binary-size(4), rest::binary>> = data, socket, state) do
     ThousandIsland.Socket.send(socket, data)
-    IO.puts("data=#{inspect(data)} socket=#{inspect(socket)} state=#{inspect(state)}")
+  
+    IO.puts("UNSUPPORTED OPT opt=#{opt}")
     {:continue, state}
   end
 end
